1ae6317687b187c433785037d13b76a99e237066
Jacob <jacobhutter1@gmail.com>
2017-03-09 16:53:32 -0600

    set_idt_entries

--- student-distrib/interrupt_table.c
+++ student-distrib/interrupt_table.c
@@ -46,9 +46,41 @@ void build_idt(){
     idt[i].present = 1;
     idt[i].seg_selector = KERNEL_CS; // used https://www.safaribooksonline.com/library/view/understanding-the-linux/0596002130/ch04s04.html
   }
-  // create entry for divide error
-  // create entry for reserved entry
-  // create entry for nmi interrupt
-  // etc...
+        /* #define SET_IDT_ENTRY(str, handler) \
+do { \
+	str.offset_31_16 = ((uint32_t)(handler) & 0xFFFF0000) >> 16; \
+		str.offset_15_00 = ((uint32_t)(handler) & 0xFFFF); \
+} while(0) */
+
+		  SET_IDT_ENTRY(idt[0],_DIVIDE_ERROR);
+  		SET_IDT_ENTRY(idt[1],_RESERVED);
+      SET_IDT_ENTRY(idt[2],_NMI_INTERRUPT);
+  		SET_IDT_ENTRY(idt[3],_BREAKPOINT);
+  		SET_IDT_ENTRY(idt[4],_OVERFLOW);
+      SET_IDT_ENTRY(idt[5],_BOUND_RANGE_EXCEEDED);
+      SET_IDT_ENTRY(idt[6],_INVALID_OPCODE);
+  		SET_IDT_ENTRY(idt[7],_DEVICE_NOT_AVAILABLE);
+  		SET_IDT_ENTRY(idt[8],_DOUBLE_FAULT);
+  		SET_IDT_ENTRY(idt[9],_COPROCESSOR_SEGMENT_OVERRUN);
+  		SET_IDT_ENTRY(idt[10],_INVALID_TSS);
+  		SET_IDT_ENTRY(idt[11],_SEGMENT_NOT_PRESENT);
+  		SET_IDT_ENTRY(idt[12],_STACK_SEGMENT_FAULT);
+  		SET_IDT_ENTRY(idt[13],_GENERAL_PROTECTION);
+  		SET_IDT_ENTRY(idt[14],_PAGE_FAULT);
+  		SET_IDT_ENTRY(idt[15],_FLOATING_POINT_ERROR);
+      SET_IDT_ENTRY(idt[16],_ALIGNMENT_CHECK);
+  		SET_IDT_ENTRY(idt[17],_MACHINE_CHECK);
+  		SET_IDT_ENTRY(idt[18],_FLOATING_POINT_EXCEPTION);
+
+  		// set entry for Real time clock
+  		SET_IDT_ENTRY(idt[real_time_clock],_RTC);
+
+  		// set entry for keyboard
+  		SET_IDT_ENTRY(idt[kbd],_KEYBOARD);
+
+  		// set entry for system calls
+  		SET_IDT_ENTRY(idt[sys_call],_SYTEM_CALL);
+
+
   return;
 }
--- student-distrib/interrupt_table.h
+++ student-distrib/interrupt_table.h
@@ -2,6 +2,8 @@
 #define _INTERRUPT_TABLE_H // double inclusion guards
 #define sys_call 0x80
 #define exception_limit 32
+#define real_time_clock 0x28
+#define kbd 0x21
 
 extern void build_idt();
 
