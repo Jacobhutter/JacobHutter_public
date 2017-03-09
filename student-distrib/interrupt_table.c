a81dc5ed88c21a28cb8f2d078da03ddaa44aab50
Jacobhutter <jacobhutter1@gmail.com>
2017-03-09 00:02:40 -0600

    added new files

---
+++ student-distrib/interrupt_table.c
@@ -0,0 +1,33 @@
+#include "interupt_table.c"
+#include "x86_desc.h"
+
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+void build_idt(){
+  // create entry for divide error
+  // create entry for reserved entry
+  // create entry for nmi interrupt
+  // etc...
+  return;
+}
---
+++ student-distrib/interrupt_table.h
@@ -0,0 +1,7 @@
+#ifndef _INTERRUPT_TABLE_H
+#define _INTERRUPT_TABLE_H // double inclusion guards
+
+extern void build_idt(); 
+
+
+#endif /* _INTERRUPT_TABLE_H */
