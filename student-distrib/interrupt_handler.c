73458375204569227d6f566a14b774ec6e68fc28
cblong2 <cam82994@gmail.com>
2017-03-12 21:53:20 -0500

    fixed interrupt_handler.c

--- student-distrib/interrupt_handler.c
+++ student-distrib/interrupt_handler.c
@@ -1,11 +1,55 @@
 /* Consult x86 ISA manual */
 /* Appendix D */
-#include "lib.h"
-
+//#include "lib.h"
+//#include "i8259.h"
 #include "interrupt_handler.h"
 
+uint32_t kbd_eoi = 1;
+//https://github.com/arjun024/mkeykernel/blob/master/keyboard_map.h
+unsigned char keyboard_map[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+    '9', '0', '-', '=', '\b',	/* Backspace */
+    '\t',			/* Tab */
+    'q', 'w', 'e', 'r',	/* 19 */
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+    '\'', '`',   0,		/* Left shift */
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+    'm', ',', '.', '/',   0,				/* Right shift */
+    '*',
+    0,	/* Alt */
+    ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+    '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+    '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
+
 void DIVIDE_ERROR() {
+    bsod();
     printf("DIVIDE_ERROR");
+    cli(); // dont want keyboard to interfere
     while(1);
 }
 
@@ -90,14 +134,8 @@ void GENERAL_PROTECTION() {
 }
 
 void PAGE_FAULT() {
-    unsigned long regVal;
+    bsod();
     printf("PAGE_FAULT");
-    asm("movl %%cr2, %0;" : "=r" (regVal) : );
-    // Prints Address that caused fault
-    if (regVal == 0)
-        printf(" Tried to access NULL pointer");
-    else
-        printf(" Address 0x%x caused PAGE_FAULT\n", regVal);
     while(1) {
     }
 
@@ -127,16 +165,44 @@ void FLOATING_POINT_EXCEPTION() {
     }
 }
 
+/* RTC() (Handler)
+ * DESCRIPTION:  Handler function called by RTC interrupt
+ * INPUTS:       None
+ * OUTPUTS:      Calls test_interrupts, which floods screen
+ * RETURNS:      None
+ * SIDE EFFECTS: Sends EOI to PIC to end interrupt
+ */
 void RTC() {
-  printf("RTC\n");
-  while(1);
+    uint32_t reg_c;
 
+    // mask only the periodic interrupt bit
+    uint32_t period_mask = 0x00000040;
+
+    reg_c = inb(RTC_DATA);
+    if((reg_c & period_mask) != 0) {
+
+        // we have found a periodic interrupt
+        test_interrupts();
+    }
+    send_eoi(RTC_IRQ);
 }
+#define KEYBOARD_ADDR 0x64
+#define KEYBOARD_PORT 0x60
 
 void KEYBOARD() {
-  printf("KEYBOARD\n");
-  while(1);
+    // write eoi
+    uint32_t status;
+    uint32_t key;
 
+    status = inb(KEYBOARD_ADDR);
+    if(status & 0x01){
+        key = inb(KEYBOARD_PORT);
+        if(key < 0)
+            return;
+        putc(keyboard_map[key]);
+    }
+
+    send_eoi(kbd_eoi); // 1 is the irq for keyboard
 }
 
 void SYSTEM_CALL() {
