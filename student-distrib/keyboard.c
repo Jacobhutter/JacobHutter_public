478a550d0d2bd1601c7eb4ff51b47c7ee6b84f93
Jacob <jacobhutter1@gmail.com>
2017-03-16 13:02:05 -0500

    uh newline doesnt work but delete does

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -2,7 +2,7 @@
 
 #include "keyboard.h"
 #include "lib.h"
-#define buffer_size 128
+#define buffer_size 128*2
 #define screen_height 25
 #define screen_width 80
 #define vga_mem 0xB8000
@@ -13,7 +13,7 @@ unsigned char frame_buffer[25*80*2];
 volatile int index;
 
 void buffer_to_screen(){
-  memcpy((void *)frame_buffer+(screen_area-(screen_width*2)),(const void *)kbd_buffer, index);
+  memcpy((void *)frame_buffer,(const void *)kbd_buffer, index);
 }
 void display_screen(){
   memcpy((void *)vga_mem,(const void *)frame_buffer,screen_area);
@@ -27,7 +27,7 @@ void display_screen(){
 void keyboard_open() {
     index = 0;
     int i =0;
-    for(i = 0; i < buffer_size*2; i++){
+    for(i = 0; i < buffer_size; i++){
       if(i %2 == 0)
         kbd_buffer[i] = ' '; // char
       else
@@ -44,6 +44,30 @@ void keyboard_open() {
 }
 
 void keyboard_write(unsigned char keypress){
+  if(index == buffer_size -2){
+    kbd_buffer[index] = '\n';
+    index = index + 2;
+    buffer_to_screen();
+    display_screen();
+    int i;
+    for(i = 0; i < buffer_size; i++){
+      if(i %2 == 0)
+        kbd_buffer[i] = ' '; // char
+      else
+        kbd_buffer[i] = green; // color
+    }
+    index = 0;
+    return;
+  }
+  if(keypress == '\b'){ // if sent in \b
+    if(index == 0) // first check if we are at index zero so no crash
+      return;
+    kbd_buffer[index-2] = ' '; // insert the blank char
+    buffer_to_screen(); // insert the buffer onto the screen
+    display_screen(); // display the screen
+    index = index - 2; // update the index count for the kbd buffer
+    return;
+  }
   kbd_buffer[index] = keypress;
   index = index + 2;
   buffer_to_screen();
