b23ed855b2faf81ce1e416a622cad00854a1ca05
Timothy Klem <tklem2@illinois.edu>
2017-03-26 21:44:00 -0500

    Fixed up comments and terminal integration for RTC

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -26,11 +26,26 @@ int32_t rtc_init() {
 	return 0;
 }
 
+/* rtc_read()
+ * DESCRIPTION:  Pauses for the next RTC clock tick
+ * INPUTS:       None
+ * OUTPUTS:      None
+ * RETURNS:      Zero, to guarantee success
+ * SIDE EFFECTS: Forces caller to wait for next clock tick
+ */
 int32_t rtc_read() {
 	rtc_wait();
 	return 0;
 }
 
+/* rtc_write()
+ * DESCRIPTION:  Sets RTC clock frequency for process
+ * INPUTS:       freq - RTC frequency, must be a power of two
+ *                      between 2 and 1024 Hz, inclusive
+ * OUTPUTS:      None
+ * RETURNS:      Zero, if freq is valid; -1 else
+ * SIDE EFFECTS: Changes RTC clock frequency
+ */
 int32_t rtc_write(int32_t freq) {
 	// quick trick to check if power of two
 	if(freq >= RTC_INIT_FREQ && freq <= RTC_BASE_FREQ && (freq & (freq-1)) == 0) {
@@ -41,10 +56,27 @@ int32_t rtc_write(int32_t freq) {
 	}
 }
 
+/* rtc_close()
+ * DESCRIPTION:  Does nothing! RTC clock will not be allowed to shut down
+ * INPUTS:       none
+ * OUTPUTS:      None
+ * RETURNS:      Zero
+ * SIDE EFFECTS: None
+ */
 int32_t rtc_close() {
 	return 0;
 }
 
+/* test_rtc()
+ * DESCRIPTION:  Tests RTC clock by attempting to print a through p
+ *               Runs one testcase at the initial frequency of 2 Hz
+ *               Then eight testcases at various frequencies
+ * INPUTS:       none
+ * OUTPUTS:      If frequency is valid, letters a through p at the RTC frequency
+ *               If invalid, prints an error message
+ * RETURNS:      Zero
+ * SIDE EFFECTS: Prints testcase lines to monitor
+ */
 void test_rtc() {
 	int freqs[RTC_TESTCASES] = {2, 16, 45, 1, 2048, -2, 8, 1024};
     int i, j; 
