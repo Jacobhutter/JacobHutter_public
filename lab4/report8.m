94763c555da374349ed61b79dee8a7c42e0b23e8
Jacobhutter <jacobhutter1@gmail.com>
2017-03-27 23:44:27 -0500

    a

--- lab4/report8.m
+++ lab4/report8.m
@@ -1,26 +1,35 @@
-
 y = load('speechsig.mat');
 x = y.x;
 xnoise = y.xnoise;
 
-N = length(xnoise);
-w = fftshift((0:N-1)/N*2*pi); % define omega as you would for FFT
-w(1:N/2) = w(1:N/2) - 2*pi;   %
+N = 128; % fft length
+w1 = hamming(N);
+w2 = rectwin(N);
+m = 2; % step size of 2
+dim1 = floor(length(xnoise) / N) + 3383; % just found the biggest value i could until breaking it
+spec1 = zeros((N/2 +1),dim1);
+spec2 = zeros((N/2 +1),dim1);
+for i=1:dim1
+xi = xnoise(((i-1)*m+1):(i-1)*m+N);
+inner2 = xi .* w2;
+inner = xi .* w1; % multiply by window
+inner = fft(inner); % take fft
+inner2 = fft(inner2);
+inner = inner(1:N/2 + 1);  % only take first n/2 + 1 elems
+inner2 = inner2(1:N/2+1);
+outer = abs(inner); % take abs value
+outer2 = abs(inner2);
+spec1(:,i) = outer;
+spec2(:,i) = outer2;
+end
+figure;
+imagesc(spec1);
+title('Spectrogram using hamming window');
+xlabel('Time');
+ylabel('Frequency');
 
-xnoisew = fftshift(fft(xnoise));
-
-h = hamming(N);
-xnoisewh = xnoisew .* h;
-
-xnoise = ifft(ifftshift(xnoisewh),N);
-%soundsc(real(xnoise));
-
-r = zeros(N,1);
-r(2000:4999,1) = rectwin(3000);
-
-xnoisewr = xnoisew .* r;
-plot(abs(xnoisew));
-xnoise = ifft(ifftshift(xnoisewr));
-
-
-%soundsc(real(xnoise));
+figure;
+imagesc(spec2);
+title('Spectrogram using rectangular window');
+xlabel('Time');
+ylabel('Frequency');
