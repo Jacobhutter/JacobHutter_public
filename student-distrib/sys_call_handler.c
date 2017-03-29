d746e2d188fd92aa02e2290f8220912c24d307d6
Jacob <jacobhutter1@gmail.com>
2017-03-29 16:40:49 -0500

    added bare bones sys call handlers

---
+++ student-distrib/sys_call_handler.c
@@ -0,0 +1,32 @@
+#include "sys_call_handler.h"
+
+int32_t HALT (uint8_t status){
+    return 0;
+}
+int32_t EXECUTE (const uint8_t* command){
+    return 0;
+}
+int32_t READ (int32_t fd, void* buf, int32_t nbytes){
+    return 0;
+}
+int32_t WRITE (int32_t fd, const void* buf, int32_t nbytes){
+    return 0;
+}
+int32_t OPEN (const uint8_t* filename){
+    return 0;
+}
+int32_t CLOSE (int32_t fd){
+    return 0;
+}
+int32_t GETARGS (uint8_t* buf, int32_t nbytes){
+    return 0;
+}
+int32_t VIDMAP (uint8_t** screen_start){
+    return 0;
+}
+int32_t SET_HANDLER (int32_t signum, void* handler_address){
+    return 0;
+}
+int32_t SIGRETURN (void){
+    return 0;
+}
---
+++ student-distrib/sys_call_handler.h
@@ -0,0 +1,5 @@
+#ifndef _SYS_CALL_HANDLER_H
+#define _SYS_CALL_HANDLER_H
+#include "wrapper.h"
+#include "x86_desc.h"
+#endif
