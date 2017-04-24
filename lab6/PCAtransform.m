3cac2219a941521daf89b9ce259f4f20f37ee555
Jacobhutter <jacobhutter1@gmail.com>
2017-04-23 23:15:49 -0500

    A

--- lab6/PCAtransform.m
+++ lab6/PCAtransform.m
@@ -1,9 +1,4 @@
 function [ x_pca ] = PCAtransform( Ux, V, x_orig )
-    %[height,width] = size(x_orig);
-    x_hat = x_orig - Ux;
-    
-   %{ for i=1:height
-   %    x_hat(i,:) = x_orig(i,:) - Ux;
-   %end
-    x_pca =  inv(V) * x_hat';
+x_hat = x_orig - Ux;
+x_pca =  x_hat * V;
 end
--- lab6/report1.m
+++ lab6/report1.m
@@ -1,11 +1,11 @@
-x = [ 1 4 -4 -3 2 5 -6];
-h = [1 2 3 4 5];
+x = [ 1 4 -4 -3 2 5 -6]';
+h = [1 2 3 4 5]';
 a = convmtx(h,length(x));
 figure;
 imagesc(a);
 title('Plot of convolution matrix');
 
-cx = x*a;
+cx = a*x;
 c = conv(x,h);
 
 figure;
