d9bb7f2c51b121ccb23e96aafc03f126d7b74104
Jacob <jacobhutter1@gmail.com>
2017-04-27 16:27:53 -0500

    added_code_to_scheduler

--- student-distrib/scheduler.c
+++ student-distrib/scheduler.c
@@ -2,22 +2,6 @@
 
 static volatile uint8_t to_be_scheduled = 0;
 #define init_PCB_addr _8Mb - _4Kb // represents head
-int get_running_list( PCB_t ** list){
-
-    unsigned char tester = get_p_mask();
-    int i = 0;
-    int retval = 0;
-    for(i=0; i<8;i++){
-        if((tester & 0x01)){
-            list[retval] = (PCB_t *) init_PCB_addr - (_4Kb * i);
-            retval++;
-        }
-        tester = tester >> 1;
-
-    }
-    return retval;
-}
-
 
 /* called by interrupt handler for pit*/
 void time_quantum(){
@@ -31,11 +15,11 @@ void time_quantum(){
     /* get list of currently running PCB's */
     PCB_t * list[8] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
     int cur_running = 0;
-    if(cur_running == 0){
+    unsigned char tester = get_p_mask();
+    if(tester == 0x00){
         sti();
         return;
     }
-    unsigned char tester = get_p_mask();
     int i = 0;
     for(i=0; i<8;i++){
         if((tester & 0x01)){
