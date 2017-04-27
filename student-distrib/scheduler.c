bba4282b6b6f5f002ca586ce6b24dc86ecb88695
Jacob <jacobhutter1@gmail.com>
2017-04-27 16:24:27 -0500

    added_code_to_scheduler

--- student-distrib/scheduler.c
+++ student-distrib/scheduler.c
@@ -1,6 +1,6 @@
 #include "scheduler.h"
 
-
+static volatile uint8_t to_be_scheduled = 0;
 #define init_PCB_addr _8Mb - _4Kb // represents head
 int get_running_list( PCB_t ** list){
 
@@ -17,6 +17,8 @@ int get_running_list( PCB_t ** list){
     }
     return retval;
 }
+
+
 /* called by interrupt handler for pit*/
 void time_quantum(){
 
@@ -26,11 +28,28 @@ void time_quantum(){
     /* allow for more pic interrupts */
     send_eoi(PIT_IRQ);
 
-    /* get top most running PCB */
-    PCB_t * list[8];
+    /* get list of currently running PCB's */
+    PCB_t * list[8] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
+    int cur_running = 0;
+    if(cur_running == 0){
+        sti();
+        return;
+    }
+    unsigned char tester = get_p_mask();
+    int i = 0;
+    for(i=0; i<8;i++){
+        if((tester & 0x01)){
+            list[cur_running] = (PCB_t *) init_PCB_addr - (_4Kb * i);
+            cur_running++;
+        }
+        tester = tester >> 1;
+    }
 
-    int cur_running = get_running_list(list);
+    PCB_t * to_run = list[to_be_scheduled];
 
+    /* TODO: context switch into to_run process */
+
+    to_be_scheduled = (to_be_scheduled +1)%cur_running;
     /* set interrupts for this processor*/
     sti();
 }
