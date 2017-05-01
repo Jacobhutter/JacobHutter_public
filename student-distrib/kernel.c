1f178a01a75bf2c3c05dbadce5445395e296ab0b
Jacob <jacobhutter1@gmail.com>
2017-05-01 16:42:52 -0500

    c_scale

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -228,24 +228,25 @@ entry (unsigned long magic, unsigned long addr)
 
 //Make a beep
 //void beep() {
+    terminal_write("Booting up!\n",12);
     uint32_t c_scale[8] = {130,147,165,175,196,220,247,262};
     uint32_t q;
-    for(q =0; q<(8*450000);q++){
+    for(q =0; q<(8*400000);q++){
         if(q<450000)
             play_sound(c_scale[0]);
-        else if(q<2*450000)
+        else if(q<2*400000)
             play_sound(c_scale[1]);
-            else if(q<3*450000)
+            else if(q<3*400000)
                 play_sound(c_scale[2]);
-                else if(q<4*450000)
+                else if(q<4*400000)
                     play_sound(c_scale[3]);
-                    else if(q<5*450000)
+                    else if(q<5*400000)
                         play_sound(c_scale[4]);
-                        else if(q<6*450000)
+                        else if(q<6*400000)
                             play_sound(c_scale[5]);
-                            else if(q<7*450000)
+                            else if(q<7*400000)
                                 play_sound(c_scale[6]);
-                                else if(q<8*450000)
+                                else if(q<8*400000)
                                     play_sound(c_scale[7]);
     }
     nosound();
