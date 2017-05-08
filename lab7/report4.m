fd5fc70e3a3e93d4fc7d3c3c4a15436bb46f6914
Jacobhutter <jacobhutter1@gmail.com>
2017-05-07 21:53:54 -0500

    a

--- lab7/report4.m
+++ lab7/report4.m
@@ -10,21 +10,29 @@ plot(xnew1);
 plot(xnew2);
 % plot shows same graph overlayed 3 times 
 
-x1 = idwt(A1(A1<mean(A1)),[],'sym4');
-x2 = idwt([],D1(D1<mean(D1)),'sym4');
-x3 = idwt(A2(A2<mean(A2)),[],'coif1');
-x4 = idwt([],D2(D2<mean(D2)),'coif1');
+A1mean = mean(A1);
+A2mean = mean(A2);
+D1mean = mean(D1);
+D2mean = mean(D2);
+A1(A1 < A1mean) = 0;
+A2(A2 < A2mean) = 0;
+D1(D1 < D1mean) = 0;
+D2(D2 < D2mean) = 0;
+x1 = idwt(A1,[],'sym4');
+x2 = idwt([],D1,'sym4');
+x3 = idwt(A2,[],'coif1');
+x4 = idwt([],D2,'coif1');
 
 figure
 subplot(411);
-plot(x1);
+stem(x1);
 title('idwt of phi sym4');
 subplot(412);
-plot(x2);
+stem(x2);
 title('idwt of psi sym4');
 subplot(413);
-plot(x3);
+stem(x3);
 title('idwt of phi coif1');
 subplot(414);
-plot(x4);
+stem(x4);
 title('idwt of psi coif1');
