c51cf63e61531e877b7a5713c9a74ce8dcf356e4
grudolp2 <grudolp2@illinois.edu>
2018-02-12 15:28:52 -0600

    Optimal path

--- mp1/part2/mstheuristic.py
+++ mp1/part2/mstheuristic.py
@@ -2,6 +2,8 @@ import sys
 import astar
 import itertools
 import copy
+from collections import defaultdict
+import heapq
 
 class successor:
 	def __init__(self, loc):
@@ -13,16 +15,26 @@ class successor:
 		self.parent = []
 		self.unvisited = set()
 
+	def __lt__(self, other):
+		if self.f < other.f:
+			return True
+		elif self.f == other.f and self.h < other.h:
+			return True
+		return False
+
 def mst(critical_points, dist_map):
     edges = []
     subgraph = []
     distance = 0
+
     for i in range(len(critical_points)):
         for j in range(i,len(critical_points)):
             if(i != j):
                 edges.append((dist_map[(critical_points[i], critical_points[j])], critical_points[i], critical_points[j])) # distance, startpoint, endpoint
+
     edges.sort(key=lambda x: x[0]) # sort by distance
     add_flag = 0
+
     for e in range(len(edges)):
         add_flag = 0
         if not (edges[e][1] in subgraph):
@@ -35,61 +47,70 @@ def mst(critical_points, dist_map):
             distance += edges[e][0]
         if len(subgraph) == len(critical_points):
             break
+
     return distance
 
 
 def nearest_unvisited(loc1, critical_points, dist_map):
     global_min = sys.maxint
+
     if(len(critical_points) == 0):
 	return 0
+
     for i in range(len(critical_points)):
         if dist_map[(loc1, critical_points[i])] < global_min:
             global_min = dist_map[(loc1, critical_points[i])]
+
     return global_min
 
 def mst_heuristic(start, end, maze):
 
 	#SEARCH CODE
+
     nodes_explored = 0
     start_successor = successor(start)
     start_successor.unvisited = list(end)
     found_end = 0
     start_successor.distance = 0
     open_list = []
-    open_list.append(start_successor)
+    heapq.heappush(open_list, start_successor)
     closed_list = []
+
     dist_map = dict()
     critical_points = end
     critical_points.insert(0, start)
+
     for i in range(0, len(critical_points)):
     	for j in range(0, len(critical_points)):
     		dist = astar.a_star(critical_points[i], critical_points[j], maze)
     		dist_map[(critical_points[i], critical_points[j])] = dist
     		dist_map[(critical_points[j], critical_points[i])] = dist
-    print(mst(critical_points, dist_map))
+
     # https://www.geeksforgeeks.org/a-search-algorithm/
     global_cost = sys.maxint
     ret_path = []
+    best_state = defaultdict(lambda: None)
     while(open_list):
 	nodes_explored += 1
-	open_list.sort(key = lambda t: t.h)
-    	q = min(open_list, key = lambda t: t.f) # pick out minimum f
-	open_list.remove(q)
+    	q = heapq.heappop(open_list) # pick out minimum f
     	q_successors = []
+
         # check goal state
-	#print(q.h)
-	#print(q.f)
-	#print("Global Cost: " + str(global_cost))
+		#print(q.h)
+		#print(q.f)
+		#print("Global Cost: " + str(global_cost))
+
         if not q.unvisited:
-	    print('done')
 	    closed_list.append(q)
 	    if q.f < global_cost:
+		print(q.f)
 		global_cost = q.f
 		ret_path = copy.copy(q.parent)
 		ret_path.append(q.location)
 	    	for x in open_list:
 			if x.f >= global_cost:
 				open_list.remove(x)
+		heapq.heapify(open_list)
 	    if not open_list:
 		break
 	    else:
@@ -98,24 +119,37 @@ def mst_heuristic(start, end, maze):
         # generate successors
         for i in range(len(q.unvisited)):
             q_successors.append(successor(q.unvisited[i]))
+
     	for s in q_successors:
+
     		s.parent = copy.copy(q.parent)
 		s.parent.append(q.location)
 		s.unvisited = set()
 		s.unvisited = copy.copy(q.unvisited)
 		s.unvisited.remove(s.location)
+
     		s.g = q.g + dist_map[(q.location, s.location)]
     		s.h = mst(s.unvisited, dist_map)
-		s.u = nearest_unvisited(s.location, s.unvisited, dist_map)
-    		s.f = s.g + s.h + s.u
+			#s.u = nearest_unvisited(s.location, s.unvisited, dist_map)
+    		s.f = s.g + s.h# + s.u
+
     		#check for lower f entries already open
-		if global_cost <= s.f:# and x.g > s.g]: #and x.h <= s.h]:
+		if global_cost <= s.f:
 			continue
-		elif[x for x in open_list if x.unvisited >= s.unvisited and x.u <= s.u and x.f < s.f and x.location == s.location]:
-			continue
-    	 	else:
-			open_list.append(s)
-		#s.unvisited.remove(s.location)
+		curr_best = best_state[(tuple(s.unvisited), s.location)]
+		if curr_best == None:
+			best_state[(tuple(s.unvisited), s.location)] = s
+			heapq.heappush(open_list, s)
+		elif best_state[(tuple(s.unvisited), s.location)].g > s.g:
+			#try:
+			#	print('test') 
+				#open_list.remove(best_state[(tuple(s.unvisited), s.location)])
+				#heapq.heapify(open_list)
+			#except ValueError:
+			#	pass
+			best_state[(tuple(s.unvisited), s.location)] = s
+			heapq.heappush(open_list, s)
+		
     return maze, global_cost, ret_path, nodes_explored
 
 maze = []
