ea9c4b64d04df9c1a11c6679698b20cfaa35f0e2
Unknown <cam82994@gmail.com>
2018-03-08 13:00:37 -0600

    2.1 so far
    All rules should be good to go except rule 4 and the first move.

--- mp2/part2/reflex.py
+++ mp2/part2/reflex.py
@@ -12,29 +12,29 @@ test1 = [[0, 0, 1, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0]]
 
-test2 = [[0, 0, 1, 0, 0, 0, 0],
+test2 = [[0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 1, 1, 0, 0],
-         [0, 1, 0, 1, 0, 0, 0],
-         [0, -1, -1, -1, -1, 0, 0],
          [0, 0, 0, 0, 0, 0, 0],
-         [0, -1, 0, -1, 0, -1, 0]]
+         [-1, 1, 1, 1, 1, 0, 0],
+         [-1, -1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0]]
 
-test3 = [[0, 0, 1, 0, 0, 0, 0],
+test3 = [[0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 1, 1, 0, 0],
-         [0, 1, 0, 1, 0, -1, 0],
-         [0, 0, 0, 0, 0, -1, 0],
-         [0, 0, 0, 0, 0, -1, 0],
-         [0, -1, 0, -1, 0, -1, 0]]
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, -1, 0, 0, 0, 0],
+         [0, 0, 1, 1, 1, 0, 0],
+         [0, -1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0]]
 
-test4 = [[0, 0, 1, 0, 0, 0, 0],
+test4 = [[0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 1, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 1, 1, 1, 0],
-         [0, 1, 0, 1, 0, 1, 0],
-         [0, 0, 0, 0, 0, 1, 0],
-         [0, 0, 0, 0, 0, 1, 0],
-         [0, -1, 0, -1, 0, -1, 0]]
+         [0, 0, -1, 0, 0, -1, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, -1, 0, 0, 0, 0, 0]]
 #[row][col]
 #print(test1[4][5])
 
@@ -51,8 +51,17 @@ for x in range(len(test2)):
 
 
 def print_board(board):
-    for row in board:
-        print row
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if(board[x][y] == red_piece):
+                print "red   ",
+            elif(board[x][y] == blue_piece):
+                print "blue   ",
+            else:
+                print (str(board[x][y]) + "     "),
+        print
+#    for row in board:
+#        print row
 
 
 def check_row(row, piece):
@@ -64,7 +73,16 @@ def check_row(row, piece):
             pieces.append(x)
         elif(row[x] == 0):
             count = 0;
-        if(count >= 4):
+        if((count >= 3) and (row[x - 3] == 0) and (row[x + 1] == 0)):
+            if(x < 6):
+                if(row[x - 3] == 0):
+                    row[x - 3] = blue_piece
+                    return row
+                else:
+                    if(row[x + 1] == 0):
+                        row[x + 1] = blue_piece
+                        return row
+        elif(count >= 4):
             if(x < 6):
                 if(row[x - 4] == 0):
                     row[x - 4] = blue_piece
@@ -84,14 +102,29 @@ def check_col(board, col, piece):
             pieces.append(y)
         elif(board[x][col] == 0):
             count = 0
-        if(count >=4):
+        if((count >= 3) and (board[x - 3][col] == 0) and (board[x + 1][col] == 0)):
             if(x < 6):
-                print(board[6][col])
                 if(board[x + 1][col] == 0):
                     board[x + 1][col] = blue_piece
                     return board
-            elif(x > 3):
-                print(board[6][col])
+                elif(x > 2):
+                    if(board[x - 3][col] == 0):
+                        board[x - 3][col] = blue_piece
+                        return board
+            elif(x > 2):
+                if(board[x - 3][col] == 0):
+                    board[x - 3][col] = blue_piece
+                    return board
+        elif(count >= 4):
+            if(x < 6):
+                if(board[x + 1][col] == 0):
+                    board[x + 1][col] = blue_piece
+                    return board
+                elif(x > 2):
+                    if(board[x - 4][col] == 0):
+                        board[x - 4][col] = blue_piece
+                        return board
+            elif(x > 2):
                 if(board[x - 4][col] == 0):
                     board[x - 4][col] = blue_piece
                     return board
@@ -118,25 +151,139 @@ def check_win(board):
                             val = check_col(board, row_val[y], blue_piece)
                             if(val != 0):
                                 board = val
-                                break
+                                return board
                 #print(cols_checked)
                 else:
                     board[x] = row_val
-                return board
+                    return board
 
+
+def check_block(board):
+    cols_checked = []
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if(board[x][y] == red_piece):
+                row_val = check_row(board[x], red_piece)
+                if(len(row_val) < 6):
+                    print("At row " + str(x) + " the output is " + str(row_val))
+                    for y in range(len(row_val)):
+                        if(not_checked(row_val[y], cols_checked)):
+                            print("Checking col " + str(row_val[y]))
+                            cols_checked.append(row_val[y])
+                            val = check_col(board, row_val[y], red_piece)
+                            if(val != 0):
+                                board = val
+                                return board
+                #print(cols_checked)
+                else:
+                    board[x] = row_val
+                    return board
+
+def getsum(block):
+    sum = 0
+    for i in range(len(block)):
+        if(block[i][2] == red_piece):#
+            sum += 1
+    return sum
+
+def verify_block(blocks, x_cord, y_cord):
+    for i in range(len(blocks)):
+        if((blocks[i][0] >= x_cord) or (blocks[i][1] >= y_cord)):
+            return 1
+    return 0
+
+
+
+# Need a way to compare the smallest block size so that the biggest is not
+def check_winblock(board):
+    cols_checked = []
+    blocks = []
+    blocks.append((6, 6, 0))
+    blocks.append((6, 6, 0))
+    blocks.append((6, 6, 0))
+    blocks.append((6, 6, 0))
+    blocks.append((6, 6, 0))
+    block = []
+    seq = 0
+    max = 0
+    print "rows"
+    for x in range(len(board)):
+        seq = 0
+        for y in range(len(board[x])):
+            if(board[x][y] != blue_piece):#
+                seq += 1
+                block.append((x, y, board[x][y]))
+                if(seq == 5):
+                    seq = 0
+                    val = getsum(block)
+                    print "VAL: " + str(val)
+                    if(max <= val):
+                        if(verify_block(blocks, x, y)):
+                            print max
+                            max = val
+                            blocks = []
+                            blocks = block
+                    block = []
+        
+            else:
+                seq = 0
+                block = []
+
+    block = []
+    seq = 0
+
+    print "cols"
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if(board[y][x] != blue_piece):#
+                seq += 1
+                block.append((y, x, board[y][x]))
+                if(seq == 5):
+                    seq = 0
+                    val = getsum(block)
+                    if(max <= val):
+                        if(verify_block(blocks, y, x)):
+                            max = val
+                            blocks = []
+                            blocks = block
+                    block = []
+            else:
+                seq = 0
+                block = []
+
+    for i in range(len(blocks)):
+        x = blocks[i][0]
+        y = blocks[i][1]
+        if(board[x][y] == 0):
+            board[x][y] = red_piece#
+            return board
 
 
 print
-print("TEST 1")
+print("Test 1")
 check_win(test1)
+check_block(test1)
+check_winblock(test1)
+print_board(test1)
+
 print
-print("TEST 2")
+print("Test 2")
 check_win(test2)
+check_block(test2)
+check_winblock(test2)
+print_board(test2)
+
 print
-print("TEST 3")
+print("Test 3")
 check_win(test3)
+check_block(test3)
+check_winblock(test3)
 print_board(test3)
+
 print
-print("TEST 4")
-#check_block(test4)
+print("Test 4")
+check_win(test4)
+check_block(test4)
+check_winblock(test4)
 print_board(test4)
+
