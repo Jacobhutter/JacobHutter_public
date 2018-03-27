81111ce728c8050325aa57a286370f260267bf6e
Jacob <jacobhutter1@gmail.com>
2018-03-27 16:30:59 -0500

    90% with confusion matrix

--- mp3/part1/part1.rb
+++ mp3/part1/part1.rb
@@ -37,9 +37,8 @@ File.open("digitdata/optdigits-orig_test.txt", "r") do |f|
         map_rule.each_with_index do |c, index|
             val = 0.0
             test_im.each_with_index do |feature, row, col|
-                if(feature == 0)
-                    #print 1-image_topography[index].element(row, col), index, "\n"
-                    val += Math.log((1-image_topography[index].element(row, col)))
+                if(feature == 1)
+                    val += Math.log(image_topography[index].element(row, col))
                 end
             end
             map_rule[index] = Matrix.rows(c.to_a << [val]) # append row to matrix for this particular class
@@ -53,13 +52,22 @@ end
 occurrences = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 guesses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 classification_accuracy = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+confusion_matrix = Array.new(10){Array.new(10, 0)}
 
 map_rule[0].each_with_index do |score, test_im_row|
     guess =  [map_rule[0].element(test_im_row,0), map_rule[1].element(test_im_row,0),map_rule[2].element(test_im_row,0),map_rule[3].element(test_im_row,0),
     map_rule[4].element(test_im_row,0),map_rule[5].element(test_im_row,0),map_rule[6].element(test_im_row,0),map_rule[7].element(test_im_row,0),map_rule[8].element(test_im_row,0),map_rule[9].element(test_im_row,0)].each_with_index.max[1]
+    confusion_matrix[guess][sol_array.element(test_im_row, 0)] += 1
+    occurrences[sol_array.element(test_im_row, 0)] += 1
+    if guess == sol_array.element(test_im_row, 0)
+        guesses[guess] += 1
+    end
+end
 
-    occurrences[sol_array.element(test_im_row,0)] += 1
-    guesses[guess] += 1
+confusion_matrix.each_with_index do |count_row, row|
+    count_row.each_with_index do |count, col|
+        confusion_matrix[row][col] = (confusion_matrix[row][col].to_f/occurrences[col]).round(4)
+    end
 end
 
 average = 0
@@ -72,5 +80,17 @@ guesses.each_with_index do |guess, index|
     end
     average += classification_accuracy[index]
 end
-print "Accuracy average: ", 10 * average, " %\n"
-print "Accuracy average with drop of outlier: ", 100*(average-min_drop)/9, " %\n"
+
+
+print "Accuracy unweighted average: ", 10 * average, " %\n"
+print "Confusion Matrix (each column is a golden value, each row is a guess value): \n"
+print confusion_matrix[0], "\n"
+print confusion_matrix[1], "\n"
+print confusion_matrix[2], "\n"
+print confusion_matrix[3], "\n"
+print confusion_matrix[4], "\n"
+print confusion_matrix[5], "\n"
+print confusion_matrix[6], "\n"
+print confusion_matrix[7], "\n"
+print confusion_matrix[8], "\n"
+print confusion_matrix[9], "\n"
