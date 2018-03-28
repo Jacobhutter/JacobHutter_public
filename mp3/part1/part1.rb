9ddccb439ab94ba55a8a5d8c2fc9b89658c4f327
Jacob <jacobhutter1@gmail.com>
2018-03-28 00:04:02 -0500

    adding color motherfucker

--- mp3/part1/part1.rb
+++ mp3/part1/part1.rb
@@ -1,4 +1,30 @@
 require 'matrix'
+# require 'colorize'
+class String
+def black;          "\e[30m#{self}\e[0m" end
+def red;            "\e[31m#{self}\e[0m" end
+def green;          "\e[32m#{self}\e[0m" end
+def brown;          "\e[33m#{self}\e[0m" end
+def blue;           "\e[34m#{self}\e[0m" end
+def magenta;        "\e[35m#{self}\e[0m" end
+def cyan;           "\e[36m#{self}\e[0m" end
+def gray;           "\e[37m#{self}\e[0m" end
+
+def bg_black;       "\e[40m#{self}\e[0m" end
+def bg_red;         "\e[41m#{self}\e[0m" end
+def bg_green;       "\e[42m#{self}\e[0m" end
+def bg_brown;       "\e[43m#{self}\e[0m" end
+def bg_blue;        "\e[44m#{self}\e[0m" end
+def bg_magenta;     "\e[45m#{self}\e[0m" end
+def bg_cyan;        "\e[46m#{self}\e[0m" end
+def bg_gray;        "\e[47m#{self}\e[0m" end
+
+def bold;           "\e[1m#{self}\e[22m" end
+def italic;         "\e[3m#{self}\e[23m" end
+def underline;      "\e[4m#{self}\e[24m" end
+def blink;          "\e[5m#{self}\e[25m" end
+def reverse_color;  "\e[7m#{self}\e[27m" end
+end
 
 image_topography = [Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32),Matrix.zero(32,32)]
 File.open("digitdata/optdigits-orig_train.txt", "r") do |f|
@@ -94,3 +120,63 @@ print confusion_matrix[6], "\n"
 print confusion_matrix[7], "\n"
 print confusion_matrix[8], "\n"
 print confusion_matrix[9], "\n"
+puts "╔═══════════════╗\n║ Starting Odds ║\n╚═══════════════╝"
+puts "╔═════════╗\n║ 8 <=> 2 ║\n╚═════════╝\n"
+prev_row = 0
+image_topography[8].each_with_index do |feature, row, col|
+    if prev_row != row
+        prev_row = row
+        print "\n"
+    end
+
+    if Math.log(feature) < (-5)
+        print '█'.blue
+    elsif Math.log(feature) < (-2)
+        print '█'.magenta
+    elsif Math.log(feature) < (-1)
+        print '█'.cyan
+    else
+        print '█'.red
+    end
+end
+print "\n\n"
+
+prev_row = 0
+image_topography[2].each_with_index do |feature, row, col|
+    if prev_row != row
+        prev_row = row
+        print "\n"
+    end
+
+    if Math.log(feature) < (-5)
+        print '█'.blue
+    elsif Math.log(feature) < (-2)
+        print '█'.magenta
+    elsif Math.log(feature) < (-1)
+        print '█'.cyan
+    else
+        print '█'.red
+    end
+end
+print "\n\n"
+
+prev_row = 0
+image_topography[2].each_with_index do |feature, row, col|
+    if prev_row != row
+        prev_row = row
+        print "\n"
+    end
+
+    cur = Math.log(feature) - Math.log(image_topography[8].element(row,col))
+    # cur
+    if cur < (0)
+        print '█'.blue
+    elsif cur < (0.2)
+        print '█'.magenta
+    elsif cur < (1)
+        print '█'.cyan
+    else
+        print '█'.red
+    end
+end
+print "\n\n"
