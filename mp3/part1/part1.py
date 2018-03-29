1d3e3fdb68e9dd3eb282996890a6cd235d6410ae
Jacobhutter <jacobhutter1@gmail.com>
2018-03-29 00:37:16 -0500

    part1 halfway done i think

--- mp3/part1/part1.py
+++ mp3/part1/part1.py
@@ -3,45 +3,36 @@ from scipy import misc
 from PIL import Image
 import numpy as np
 import math
+import scipy
 import sys
 import harris
+import cv2
 
+# https://docs.opencv.org/3.1.0/da/df5/tutorial_py_sift_intro.html
+# plus a lot of freaking googling to realize i needed opencv-contrib aswell for xfeatures2d........ >:(
+l_im = cv2.imread('images/uttower_left.jpg')
+l_gray = cv2.cvtColor(l_im, cv2.COLOR_BGR2GRAY)
+r_im = cv2.imread('images/uttower_right.jpg')
+r_gray = cv2.cvtColor(r_im, cv2.COLOR_BGR2GRAY)
 
-def get_neighbor_coords(im, x, y, X, Y):
+l_mask = np.ones((l_im.shape[1], l_im.shape[0]), np.uint8)
+l_mask[:((l_im.shape[1]/2))] = 0
+l_mask = np.transpose(l_mask)
 
-    neighbors = lambda x, y : [l_im[x2][y2] for x2 in range(x-1, x+2)
-                                   for y2 in range(y-1, y+2)
-                                   if (-1 < x <= X and
-                                       -1 < y <= Y and
-                                       (0 <= x2 <= X) and
-                                       (0 <= y2 <= Y))]
-    coords = neighbors(x,y)
-    return neighbors(x, y)
+r_mask = np.ones((r_im.shape[1], r_im.shape[0]), np.uint8)
+r_mask[(r_im.shape[1]/2):] = 0
+r_mask = np.transpose(r_mask)
 
-def GetImage(filename):
-    # create jpg of from file name
-    try:
-        img = Image.open(filename)
-    except IOError, ioe:
-        print("That file Does not exist")
-        sys.exit();
-    return img
+l_sift = cv2.xfeatures2d.SIFT_create(nfeatures=200)
+l_kp = l_sift.detect(l_gray, mask=l_mask)
+l_kp, l_des = l_sift.compute(l_gray, l_kp)
 
-def GetImages(str1, str2):
-    return np.array(GetImage(str1).convert('L')), np.array(GetImage(str2).convert('L'))
+r_sift = cv2.xfeatures2d.SIFT_create(nfeatures=200)
+r_kp= r_sift.detect(r_gray, mask=r_mask)
+r_kp, r_des = r_sift.compute(r_gray, r_kp)
 
-[l_im, r_im] = GetImages('images/uttower_left.jpg', 'images/uttower_right.jpg')
-harris_l_im = harris.compute_harris_response(l_im)
-harris_r_im = harris.compute_harris_response(r_im)
-l_coords = harris.get_harris_points(harris_l_im)
-r_coords = harris.get_harris_points(harris_r_im)
-
-neighborhood_coords_l = np.empty((0,0))
-for i in range(len(l_coords)):
-    print l_coords[i], '\n', neighborhood_coords_l
-    if i == 0:
-        neighborhood_coords_l = get_neighbor_coords(l_im, l_coords[i][0], l_coords[i][1], l_im.shape[0], l_im.shape[1])
-    else:
-        neighborhood_coords_l.append(get_neighbor_coords(l_im, l_coords[i][0], l_coords[i][1], l_im.shape[0], l_im.shape[1]))
-
-#print neighborhood_coords_l
+dist_hash = {}
+for i in range(l_des.shape[0]):
+    print i
+    for j in range(r_des.shape[0]):
+        dist_hash[(i, j)] = scipy.spatial.distance.cdist(zip(l_des[i]), zip(r_des[j]), 'sqeuclidean')
