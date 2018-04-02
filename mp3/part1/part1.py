3d59ee6f585a14f475851b05ac1baf7f5f073c8a
Jacob <jacobhutter1@gmail.com>
2018-04-02 14:29:56 -0500

    adding wip

--- mp3/part1/part1.py
+++ mp3/part1/part1.py
@@ -1,6 +1,7 @@
 from scipy import signal
 from scipy import misc
 from PIL import Image
+from matplotlib import pyplot as plt
 import numpy as np
 import math
 import scipy
@@ -28,28 +29,49 @@ l_sift = cv2.xfeatures2d.SIFT_create(nfeatures=100)
 l_kp = l_sift.detect(l_gray, mask=l_mask)
 l_kp, l_des = l_sift.compute(l_gray, l_kp)
 
-
-
 r_sift = cv2.xfeatures2d.SIFT_create(nfeatures=100)
 r_kp= r_sift.detect(r_gray, mask=r_mask)
 r_kp, r_des = r_sift.compute(r_gray, r_kp)
-
-img=cv2.drawKeypoints(r_gray, r_kp, r_im)
-cv2.imwrite('sift_keypoints.jpg',img)
 print "hashing descriptor average distances..."
 reverse_hash = {}
 dist_hash = {}
+
 for i in range(l_des.shape[0]):
     for j in range(r_des.shape[0]):
         dist_hash[(i, j)] = np.mean(np.array(scipy.spatial.distance.cdist(zip(l_des[i]), zip(r_des[j]), 'sqeuclidean'))) # returns mean of dist_mat
         reverse_hash[dist_hash[(i, j)]] = (i, j)
-n = 300 # get 300 'good' keypoints
+
+n = 100 # get 100 'good' keypoints
 print "finding the ", n, "th smallest mean distance descriptors..."
 vals = np.array(dist_hash.values())
-
 n_min_vals = vals.argsort()[:n]
-n_min_descriptors = np.empty((0,0))
-for i in range(len(n_min_vals)):
-    n_min_descriptors = np.append(n_min_descriptors, (reverse_hash[vals[n_min_vals[i]]][0], reverse_hash[vals[n_min_vals[i]]][1]))
+left_good_keypoints = []
+right_good_keypoints = []
 
 print "running ransac on 'good' keypoints..."
+#https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_feature_homography/py_feature_homography.html
+for i in range(len(n_min_vals)):
+    left_good_keypoints.append(np.float32((l_kp[reverse_hash[vals[n_min_vals[i]]][0]].pt[0], l_kp[reverse_hash[vals[n_min_vals[i]]][0]].pt[1])))
+    right_good_keypoints.append(np.float32((r_kp[reverse_hash[vals[n_min_vals[i]]][1]].pt[0], r_kp[reverse_hash[vals[n_min_vals[i]]][1]].pt[1])))
+
+left_good_keypoints =  np.asarray(left_good_keypoints)
+right_good_keypoints = np.asarray(right_good_keypoints)
+
+M, mask = cv2.findHomography(left_good_keypoints, right_good_keypoints, cv2.RANSAC, 5.0)
+matchesMask = mask.ravel().tolist()
+
+h = l_im.shape[0]
+w = l_im.shape[1]
+pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+dst = cv2.perspectiveTransform(pts, M)
+
+img2 = cv2.polylines(r_im,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(l_im,l_kp,img2,r_kp, matchesMask ,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
