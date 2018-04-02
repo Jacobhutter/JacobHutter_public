917095e7f29f5002767fabfc962eb4ae3f1554ba
Jacobhutter <jacobhutter1@gmail.com>
2018-04-02 15:35:02 -0500

    a

--- mp3/part1/part1.py
+++ mp3/part1/part1.py
@@ -1,6 +1,7 @@
 from scipy import signal
 from scipy import misc
-from PIL import Image
+from PIL import Image, ImageOps
+from skimage import transform
 from matplotlib import pyplot as plt
 import numpy as np
 import math
@@ -10,6 +11,7 @@ import harris
 import cv2
 
 # https://docs.opencv.org/3.1.0/da/df5/tutorial_py_sift_intro.html
+# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_feature_homography/py_feature_homography.html
 # plus a lot of freaking googling to realize i needed opencv-contrib as well for xfeatures2d........ >:(
 print "reading images..."
 l_im = cv2.imread('images/uttower_left.jpg')
@@ -25,53 +27,55 @@ r_mask = np.ones((r_im.shape[1], r_im.shape[0]), np.uint8)
 r_mask[(r_im.shape[1]/2):] = 0
 r_mask = np.transpose(r_mask)
 
-l_sift = cv2.xfeatures2d.SIFT_create(nfeatures=100)
-l_kp = l_sift.detect(l_gray, mask=l_mask)
+l_sift = cv2.xfeatures2d.SIFT_create()
+l_kp = l_sift.detect(l_gray)
 l_kp, l_des = l_sift.compute(l_gray, l_kp)
 
-r_sift = cv2.xfeatures2d.SIFT_create(nfeatures=100)
-r_kp= r_sift.detect(r_gray, mask=r_mask)
+r_sift = cv2.xfeatures2d.SIFT_create()
+r_kp= r_sift.detect(r_gray)
 r_kp, r_des = r_sift.compute(r_gray, r_kp)
-print "hashing descriptor average distances..."
-reverse_hash = {}
-dist_hash = {}
 
-for i in range(l_des.shape[0]):
-    for j in range(r_des.shape[0]):
-        dist_hash[(i, j)] = np.mean(np.array(scipy.spatial.distance.cdist(zip(l_des[i]), zip(r_des[j]), 'sqeuclidean'))) # returns mean of dist_mat
-        reverse_hash[dist_hash[(i, j)]] = (i, j)
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
 
-n = 100 # get 100 'good' keypoints
-print "finding the ", n, "th smallest mean distance descriptors..."
-vals = np.array(dist_hash.values())
-n_min_vals = vals.argsort()[:n]
-left_good_keypoints = []
-right_good_keypoints = []
+flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-print "running ransac on 'good' keypoints..."
-#https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_feature_homography/py_feature_homography.html
-for i in range(len(n_min_vals)):
-    left_good_keypoints.append(np.float32((l_kp[reverse_hash[vals[n_min_vals[i]]][0]].pt[0], l_kp[reverse_hash[vals[n_min_vals[i]]][0]].pt[1])))
-    right_good_keypoints.append(np.float32((r_kp[reverse_hash[vals[n_min_vals[i]]][1]].pt[0], r_kp[reverse_hash[vals[n_min_vals[i]]][1]].pt[1])))
+matches = flann.knnMatch(l_des, r_des,k=2)
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
 
-left_good_keypoints =  np.asarray(left_good_keypoints)
-right_good_keypoints = np.asarray(right_good_keypoints)
+src_pts = np.float32([ l_kp[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+dst_pts = np.float32([ r_kp[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-M, mask = cv2.findHomography(left_good_keypoints, right_good_keypoints, cv2.RANSAC, 5.0)
+M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
 matchesMask = mask.ravel().tolist()
 
-h = l_im.shape[0]
-w = l_im.shape[1]
+h = l_gray.shape[0]
+w = l_gray.shape[1]
 pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts, M)
+dst = cv2.perspectiveTransform(pts,M)
 
-img2 = cv2.polylines(r_im,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+r_gray_new = cv2.polylines(r_gray,[np.int32(dst)],True,255,3, cv2.LINE_AA)
 
 draw_params = dict(matchColor = (0,255,0), # draw matches in green color
                    singlePointColor = None,
                    matchesMask = matchesMask, # draw only inliers
                    flags = 2)
 
-img3 = cv2.drawMatches(l_im,l_kp,img2,r_kp, matchesMask ,None,**draw_params)
+img3 = cv2.drawMatches(l_gray,l_kp,r_gray_new,r_kp,good,None,**draw_params)
 
-plt.imshow(img3, 'gray'),plt.show()
+# plt.imshow(img3, 'gray'),plt.show()
+
+img_with_border = ImageOps.expand(l_gray,border=300,fill='black')
+cv2.imwrite('big.png', img_with_border)
+
+estimated_transform = transform.ProjectiveTransform(M)
+warped_im_r= 256 * transform.warp(cv2.cvtColor(r_im, cv2.COLOR_BGR2GRAY), estimated_transform)
+cv2.imwrite('right.png', warped_im_r)
+
+estimated_transform = transform.ProjectiveTransform(np.linalg.inv(M))
+warped_im_l = 256 * transform.warp(l_gray, estimated_transform)
+cv2.imwrite('left.png', l_gray)
