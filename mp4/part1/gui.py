22d866ca53123d87c371d9c1e718298106b428a4
Jacob <jacobhutter1@gmail.com>
2018-04-22 21:01:28 -0500

    adding wip

--- mp4/part1/gui.py
+++ mp4/part1/gui.py
@@ -4,6 +4,9 @@
 from Tkinter import *
 import random
 import time
+import math
+import part1
+
 
 # Define ball properties and functions
 class Ball:
@@ -26,14 +29,14 @@ class Ball:
         if pos[0] <= 0:
             self.xspeed = -self.xspeed
         if self.hit_paddle(pos) == True:
-            self.yspeed = self.yspeed + 15 * random.randrange(-1, 1)
+            self.yspeed = self.yspeed + 15 * random.randrange(-1, 2)
 
             if self.yspeed < -500:
                 self.yspeed = -500
             if self.yspeed > 500:
                 self.yspeed = 500
 
-            self.xspeed = -self.xspeed + 7.5 * random.randrange(-1, 1)
+            self.xspeed = -self.xspeed + 7.5 * random.randrange(-1, 2)
             if self.xspeed <= 0 and self.xspeed > -15:
                 self.xspeed = -15
             if self.xspeed > 0 and self.xspeed < 15:
@@ -66,6 +69,14 @@ class Ball:
                 return True
         return False
 
+    def reset(self, canvas, color, size, paddle):
+        pos = self.canvas.coords(self.paddle.id)
+        self.canvas.move(self.id, 245 - pos[0], 100 - pos[1])
+        self.xspeed = 15 # initial is .03 * 500
+        self.yspeed = 5 # initial is .01 * 500
+        self.hit_right = False
+        self.score = 0
+
 # Define paddle properties and functions
 class Paddle:
     def __init__(self, canvas, color):
@@ -73,8 +84,8 @@ class Paddle:
         self.id = canvas.create_rectangle(0,0, 10, 100, fill=color)
         self.canvas.move(self.id, 490, 200)
         self.yspeed = 0
-        self.canvas.bind_all('<KeyPress-Up>', self.move_up)
-        self.canvas.bind_all('<KeyPress-Down>', self.move_down)
+        #self.canvas.bind_all('<KeyPress-Up>', self.move_up)
+        #self.canvas.bind_all('<KeyPress-Down>', self.move_down)
 
     def draw(self):
         self.canvas.move(self.id, 0, self.yspeed)
@@ -85,16 +96,21 @@ class Paddle:
         if pos[1] >= 400:
             self.yspeed = 0
 
-    def move_up(self, evt):
+    def move_up(self):
         pos = self.canvas.coords(self.id)
         if pos[1] > 0:
             self.yspeed = -20
-    def move_down(self, evt):
+    def move_down(self):
         pos = self.canvas.coords(self.id)
         if pos[1] < 400:
             self.yspeed = 20
+    def reset(self, canvas, color):
+        pos = self.canvas.coords(self.id)
+        self.canvas.move(self.id, 0, 200-pos[1])
+        self.yspeed = 0
 
 # Create window and canvas to draw on
+
 tk = Tk()
 tk.title("AI Pong")
 canvas = Canvas(tk, width=500, height=500, bd=0, bg='white')
@@ -105,15 +121,29 @@ paddle = Paddle(canvas, 'black')
 ball = Ball(canvas, 'red', 25, paddle)
 time.sleep(1.0)
 # Animation loop
-while ball.hit_right == False:
-    ball.draw()
-    paddle.draw()
-    canvas.itemconfig(label, text="Score: "+str(ball.score))
-    tk.update_idletasks()
-    tk.update()
-    time.sleep(0.05)
 
-# Game Over
-go_label = canvas.create_text(250,200,text="GAME OVER",font=("Helvetica",30))
-tk.update()
-time.sleep(3.0)
+while True:
+    while ball.hit_right == False:
+        ball.draw()
+        paddle.draw()
+        canvas.itemconfig(label, text="Score: "+str(ball.score))
+        tk.update_idletasks()
+        tk.update()
+        discrete_paddle = math.floor(12 * paddle.canvas.coords(paddle.id)[1] / (500 - 100))
+        if paddle.canvas.coords(paddle.id)[1] == 400:
+            discrete_paddle = 11
+        ball_pos = ball.canvas.coords(ball.id)
+        ball_speed = (ball.xspeed, ball.yspeed)
+        move = part1.get_move(discrete_paddle, ball_pos, ball_speed)
+        if move > 0:
+            paddle.move_up()
+        elif move < 0:
+            paddle.move_down()
+        time.sleep(0.05)
+
+    paddle.reset(canvas, 'black')
+    paddle.draw()
+    ball.reset(canvas, 'red', 25, paddle)
+    tk.update()
+    time.sleep(1.0)
+    print "done resetting"
--- mp4/part1/part1.py
+++ mp4/part1/part1.py
@@ -1,4 +1,27 @@
 import state as s
+import random
+import numpy as np
+
+Gameboard = np.zeros((12, 12))
+
+def get_move(discrete_paddle_pos, ball_pos, ball_speed):
+    print discrete_paddle_pos, ball_pos, ball_speed
+    ballxspeed = ball_speed[0]
+    ballyspeed = ball_speed[1]
+
+    if ballxspeed < 0: # discretize x speed
+        ballxspeed = -1
+    else:
+        ballxspeed = 1
+
+    if abs(ballyspeed) < 7.5: # discretize y speed
+        ballyspeed = 0
+    elif ballyspeed > 0:
+        ballyspeed = 1
+    else:
+        ballyspeed = -1
+    print ballxspeed, ballyspeed
+    return random.randrange(-1,2)
 
 Pong = s.State()
 
